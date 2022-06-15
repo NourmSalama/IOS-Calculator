55f836ae06018365d9003baab73144bca78bcbf7
Nour Salama <n.salama.ict@hotmail.com>
2022-06-15 11:38:56 +0200

    Making functions for action buttons

--- CaclApp/ViewController.swift
+++ CaclApp/ViewController.swift
@@ -8,13 +8,89 @@ class ViewController: UIViewController
         super.viewDidLoad()
     }
 
+    @IBOutlet weak var calcDiplay: UILabel!
     
-    @IBAction func clearButton(_ sender: Any) {
+    @IBAction func clearButton(_ sender: Any)
+    {
+        
     }
     
-    
-    @IBAction func devideButton(_ sender: Any) {
+    @IBAction func devideButton(_ sender: Any)
+    {
+        
+    }
+    @IBAction func multiplyButton(_ sender: Any) {
+        
     }
     
+    @IBAction func minusButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func addButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func equalButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func decimalButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func zeroButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func oneButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func twoButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func threeButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func fourButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func fiveButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func sixButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func sevenButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func eightButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func nineButton(_ sender: Any)
+    {
+        
+    }
 }
 
